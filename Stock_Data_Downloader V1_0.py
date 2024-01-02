cddeb31b43af4e58043f3c1e1982a7e51e00f030
jaketimm <154553278+jaketimm@users.noreply.github.com>
2024-01-02 16:46:55 -0500

    Removed ticker hardcoding
    Added the ability to search Yahoo Finance for any ticker and added URL exception handling

--- Stock_Data_Downloader V1_0.py
+++ Stock_Data_Downloader V1_0.py
@@ -11,33 +11,10 @@ The data is stored in the 'Yahoo_Data' table within the 'Stock_Data' DB.
 
 # Import packages
 import pandas as pd
+import urllib.request, urllib.error
 from urllib.request import urlretrieve
 import sqlite3
 import time
-
-valid_tickers = ['SPY', 'IWM', 'QQQ', '^VIX', 'UVXY', 'USO', 'NVDA', 'TSLA', 'AMZN', 'META', 'GOOG', 'MSFT',
-                 'AAPL']  # list of accepted tickers for user input. Add tickers as needed
-
-'''
-Function: read_stock_input
-Inputs: None
-Outputs: Stock Ticker for data download
-Description: Takes a user input from the console window and checks to see if it matches a list of pre-defined tickers. 
-Returns the ticker if there is a match
-'''
-def read_stock_input():
-    valid_input = False  # input flag
-
-    print('Available Tickers: ' + str(valid_tickers))  # display all tickers that are available
-
-    while not valid_input:
-        user_input = input('Enter a stock ticker with no dollar sign in all caps: ')
-        if user_input in valid_tickers:  # user entered a valid stock ticker, end input loop
-            valid_input = True
-        else:
-            print('Error: Invalid Ticker')
-
-    return user_input
 
 
 '''
@@ -85,7 +62,7 @@ Main Loop
 '''
 while 1:
 
-    Stock_Ticker = read_stock_input()  # Read ticker and timerange to download from user
+    Stock_Ticker = input('Enter a stock ticker in all caps with No $ symbol: ')  # Read ticker and timerange to download from user
     num_days = read_period_input()
 
     period_2 = int(time.time())  # fetch today's date for calculating Yahoo Finance Unix time stamp
@@ -95,17 +72,30 @@ while 1:
     Stock_url = Stock_url + Stock_Ticker
     Stock_url = Stock_url + '?period1=' + str(period_1) + '&period2=' + str(period_2) + '&interval=1d&events=history&includeAdjustedClose=true'
 
-    urlretrieve(Stock_url, 'Downloaded_Data.csv')  # download the ticker CSV from yahoo finance and save locally
-    Stock_Data = pd.read_csv('Downloaded_Data.csv')  # import the saved data into a dataframe
-    Stock_Data = Stock_Data.iloc[:, [0, 1, 2, 3, 4, 6]]  # remove the adj close column
-    Stock_Data.insert(0, 'Ticker', Stock_Ticker)  # append the Ticker to the beginning of the DF
+    try:
+        # check the Yahoo Finance URL to see if the link is functional
+        conn = urllib.request.urlopen(Stock_url)
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            # ticker does not exist on Yahoo Finance
+            print('Error: Invalid ticker entered')
+    except urllib.error.URLError as e:
+        # a different connection error has occurred
+        print('A connection error has occurred')
+    else:
+        # ticker is valid
+        conn.close()  # close the previously opened connection
+        urlretrieve(Stock_url, 'Downloaded_Data.csv')  # download the ticker CSV from yahoo finance and save locally
+        Stock_Data = pd.read_csv('Downloaded_Data.csv')  # import the saved data into a dataframe
+        Stock_Data = Stock_Data.iloc[:, [0, 1, 2, 3, 4, 6]]  # remove the adj close column
+        Stock_Data.insert(0, 'Ticker', Stock_Ticker)  # append the Ticker to the beginning of the DF
 
-    existing_data = pd.read_sql('SELECT * FROM Yahoo_Data', conx)  # read existing data and merge with new data
-    merged_data = pd.concat([Stock_Data, existing_data], ignore_index=True)
-    merged_data.drop_duplicates(subset=['Ticker', 'Date'], inplace=True)  # remove duplicate entries based on Ticker and date
+        existing_data = pd.read_sql('SELECT * FROM Yahoo_Data', conx)  # read existing data and merge with new data
+        merged_data = pd.concat([Stock_Data, existing_data], ignore_index=True)
+        merged_data.drop_duplicates(subset=['Ticker', 'Date'], inplace=True)  # remove duplicate entries based on Ticker and date
 
-    merged_data.to_sql('Yahoo_Data', conx, if_exists='replace', index=False)  # insert DF into the Yahoo Data table
-    print('Stock data imported successfully!')
+        merged_data.to_sql('Yahoo_Data', conx, if_exists='replace', index=False)  # insert DF into the Yahoo Data table
+        print('Stock data imported successfully!')
 
 
 
